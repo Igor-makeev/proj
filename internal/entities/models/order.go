4357f46821f4d6b87d2e94768f2b10f151c1791f
Igor-Makeev <Igormakeev2017@gmail.com>
2023-02-23 02:10:58 +1000

    fix

--- internal/entities/models/order.go
+++ internal/entities/models/order.go
@@ -6,7 +6,7 @@ import (
 
 type OrderDTO struct {
 	UserID     int       `json:"-"`
-	Number     string    `json:"number,string"`
+	Number     string    `json:"number"`
 	Status     string    `json:"status"`
 	Accrual    float64   `json:"accrual,omitempty"`
 	UploadedAt time.Time `json:"uploaded_at"`
