99fb96a1402872f147b47aecf1f03d0d2ef7a290
Igor-Makeev <Igormakeev2017@gmail.com>
2023-02-22 15:41:01 +1000

    fix

--- internal/repository/postgress/loyalty_service_postgress.go
+++ internal/repository/postgress/loyalty_service_postgress.go
@@ -80,7 +80,7 @@ func (sp *StoragePostgress) GetOrders(ctx context.Context, id int) ([]models.Ord
 
 func (sp *StoragePostgress) GetBalance(ctx context.Context, id int) (*models.UserBallance, error) {
 	var balance models.UserBallance
-	err := sp.db.QueryRow(context.Background(), "select current, withdrawn from users_table	where id=$1;", id).Scan(&balance.Current, &balance.Withdrawn)
+	err := sp.db.QueryRow(context.Background(), "select current_ballance, withdrawn from users_table	where id=$1;", id).Scan(&balance.Current, &balance.Withdrawn)
 	if err != nil {
 		return nil, err
 	}
